a4ba762aa8dd668824bfc5728ecf1c7bb8c69bf5
henry-150 <dsharsh150@gmail.com>
2025-12-13 11:44:07 +0530

    deployment settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/password-manager/',
   plugins: [
     react(),
     tailwindcss(),
